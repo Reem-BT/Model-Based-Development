96beeed88fb0763f8a2bc039e7e42ef25cc7ba5e
Reem-BT <ReemBahgatAbdel78_sd@nsst.bsu.edu.eg>
2026-09-23 15:43:41 +0300

    Add files via upload

---
+++ PID Controller Design/Controller gains effect on responce/ki_effect_on_responce_2nd_order.m
@@ -0,0 +1,54 @@
+
+
+num=1;
+den=[1 5 6];
+
+sys=tf(num,den);
+
+t=0:0.01:10;
+
+ki_values= [0.001 ,0.01 ,0.1 ,1 10 ,50,100];
+
+
+figure;
+
+OpenLoopRes= step(sys,t);
+plot(t,OpenLoopRes,'k--','LineWidth',1.5);
+
+hold on
+
+ClosedLoopResWithoutController=step(sys/(1+sys),t);
+plot(t,ClosedLoopResWithoutController,'b--','LineWidth',1.5);
+
+hold on
+
+for i = 1: length(ki_values)
+    ki=ki_values(i);
+   I_controller = tf(ki,[1 0]);
+   SysWithController = series(I_controller , sys);
+    closedLoopSysWithController = feedback(SysWithController,1 );
+    
+    SysResWithController = step(closedLoopSysWithController ,t);
+   
+    plot(t,SysResWithController,'LineWidth',1.5);
+    
+    hold on 
+end
+
+xlim([0 ,100]);
+
+title ('Effect of Ki on Sytem Responcee (second order)');
+xlabel('Time');
+ylabel('SysResponsce');
+
+legend (['Open Loop';'Closed Loop Without Controller';cellstr(num2str(ki_values'))]);
+
+
+
+
+
+
+
+
+
+
---
+++ PID Controller Design/Controller gains effect on responce/kp_effect_on_responce_2nd_order.m
@@ -0,0 +1,51 @@
+
+
+num=1;
+den=[1 5 6];
+
+sys=tf(num,den);
+
+t=0:0.01:10;
+
+kp_values= [0.001 ,0.01 ,0.1 ,1 10 ,50,100];
+
+
+figure;
+
+OpenLoopRes= step(sys,t);
+plot(t,OpenLoopRes,'k--','LineWidth',1.5);
+
+hold on
+
+ClosedLoopResWithoutController=step(sys/(1+sys),t);
+plot(t,ClosedLoopResWithoutController,'b--','LineWidth',1.5);
+
+hold on
+
+for i = 1: length(kp_values)
+    kp=kp_values(i);
+    SysWithController = feedback( kp * sys,1);
+    SysResWithController = step(SysWithController ,t);
+    plot(t,SysResWithController,'LineWidth',1.5);
+    
+    hold on 
+end
+
+xlim([0 ,10]);
+ylim([0, 1.2]);
+
+title ('Effect of Kp on Sytem Responcee (second order)');
+xlabel('Time');
+ylabel('SysResponsce');
+
+legend (['Open Loop';'Closed Loop Without Controller';cellstr(num2str(kp_values'))]);
+
+
+
+
+
+
+
+
+
+
